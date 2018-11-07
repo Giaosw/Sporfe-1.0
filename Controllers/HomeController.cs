6ff9e196ad12a86d67c35ad13dcade5d6f209ee9
Giaosw <giaosw@hotmail.com>
2018-11-07 15:43:42 -0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sporfe_1._0.Models;
+
+namespace Sporfe_1._0.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/IndexInsideController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+namespace Sporfe_1._0.Controllers
+{
+    public class IndexInsideController : Controller
+    {
+        public IActionResult IndexIs()
+        {
+            return View();
+        }
+                public IActionResult Proinfo()
+        {
+            return View();
+        }
+    }
+}
